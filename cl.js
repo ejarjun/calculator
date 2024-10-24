0afe11d84183d33f5aa7427405e402f480d63a5c
ejarjun <arjunej555@gmail.com>
2024-10-24 12:15:54 +0530

    add

--- cl.js
+++ cl.js
@@ -1,16 +1,29 @@
-function appendToDisplay(value) {
-    document.getElementById("display").value += value;
-}
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('button'));
 
-function clearDisplay() {
-    document.getElementById("display").value = '';
-}
-
-function calculateResult() {
-    const display = document.getElementById("display");
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = 'Error';
-    }
-}
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        let value = e.target.getAttribute('data-value');
+        
+        switch(value) {
+            case 'C':
+                display.value = '';
+                break;
+            case '+/-':
+                display.value = display.value ? parseFloat(display.value) * -1 : '';
+                break;
+            case '%':
+                display.value = display.value ? parseFloat(display.value) / 100 : '';
+                break;
+            case '=':
+                try {
+                    display.value = eval(display.value);
+                } catch {
+                    display.value = 'Error';
+                }
+                break;
+            default:
+                display.value += value;
+        }
+    });
+});
